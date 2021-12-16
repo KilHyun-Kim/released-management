cf4376571accea70b61f2c148075b030c54dd9d5
KilHyun-Kim <rlaarlfgus@gmail.com>
2021-12-16 22:39:02 +0900

    Write the dummy

---
+++ src/library/api/dummy.ts
@@ -0,0 +1,27 @@
+
+export const version = [
+    {
+        "version_id": 1,
+        "tech_id": 1,
+        "title": "React First Version",
+        "sutitle": "React First Subtitle",
+        "contents":" React First Contents",
+        "link": "React First Link"
+    },
+    {
+        "version_id": 2,
+        "tech_id":1,
+        "title": "React Second Version",
+        "sutitle": "React Second Subtitle",
+        "contents":" React Second Contents",
+        "link": "React Second Link"
+    },
+    {
+        "version_id": 3,
+        "tech_id": 1,
+        "title": "React Third Version",
+        "sutitle": "React Third Subtitle",
+        "contents":" React Third Contents",
+        "link": "React Third Link"
+    },
+]
